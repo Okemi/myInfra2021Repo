c293e9cdbde2598f554777830abd38a78c278953
Okemi <120521746+Okemi@users.noreply.github.com>
2023-05-14 13:43:13 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-dev-tf-state-bucket-2023"
-    key = "main"
+    key = "myfirstkey"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
